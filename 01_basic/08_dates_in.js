f79924d267d4f63677227b11cb9b77db924d9e55
Prateekkumar333 <166527128+Prateekkumar333@users.noreply.github.com>
2024-07-20 13:45:57 +0000

    dates in js

---
+++ 01_basic/08_dates_in.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2024, 4, 23);
+// let myCreatedDate = new Date(2024, 4, 23, 5, 3);
+let myCreatedDate = new Date("2024-05-23");
+console.log(myCreatedDate.toLocaleString());
+
+to be start from timeStamps
