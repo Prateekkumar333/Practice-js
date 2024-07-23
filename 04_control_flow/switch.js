8a24773fc38e14931853172e7e64162b4f5db5cb
Prateekkumar333 <166527128+Prateekkumar333@users.noreply.github.com>
2024-07-23 01:25:55 +0000

    switch statement for keys

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "ma"
+
+switch (month) {
+    case "ja":
+        console.log("january");
+        break;
+    case "fe":
+        console.log("feb");
+        break;
+    case "ma":
+        console.log("march");
+        break;
+    case "ap":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
