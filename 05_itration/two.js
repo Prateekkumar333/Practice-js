f853798546f4a2e8a577cef0fab4c46ecdb09d8b
Prateekkumar333 <166527128+Prateekkumar333@users.noreply.github.com>
2024-07-27 10:00:31 +0000

    while and doWhile loop

---
+++ 05_itration/two.js
@@ -0,0 +1,23 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(index);
+//     index = index + 2
+// }
+// // expected output = 0 , 2 , 4, 6, 8, 10
+
+// let myArray = [ "flash", "superman", "batman"]
+// let superHeroes=0
+// while (superHeroes < myArray.length) {
+//     console.log(myArray[superHeroes]);
+//     superHeroes++
+// }
+
+
+let score = 11
+
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
