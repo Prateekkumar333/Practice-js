ca39188ca1f25ff63b08e60eabd59afb517232c4
Prateekkumar333 <166527128+Prateekkumar333@users.noreply.github.com>
2024-07-28 02:31:36 +0000

    for in

---
+++ 05_itration/four.js
@@ -0,0 +1,16 @@
+const myObject = {
+    js : "javaScript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(myObject[key]);
+}
+
+const programming= ["js", "rb", "py"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
